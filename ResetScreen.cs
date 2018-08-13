4b945823b71e3889a338cff2ffca74534d1d1a08
Praven P Jose <pravenpjose@gmail.com>
2018-08-13 09:34:42 +0530

    Add files via upload

---
+++ ResetScreen.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ResetScreen : MonoBehaviour {
+
+
+    public bool recenterButton;
+    public Text count;
+
+    void Start ()
+    {
+        recenterButton = true;
+    }
+
+
+    public void Recenter()
+    {
+        if(recenterButton)
+        {
+            recenterButton = false;
+            DisplayText("3");
+            StartCoroutine(RecenterCountdown());
+        }
+    }
+  
+    IEnumerator RecenterCountdown()
+    {
+        int seconds = 3;
+        while (seconds > 0)
+        {
+            DisplayText(seconds.ToString());
+            yield return new WaitForSeconds(1);
+            seconds--;
+        }
+        OVRManager.display.RecenterPose();
+        DisplayText("Reset");
+        recenterButton = true;
+    }
+
+    void DisplayText(string s)
+    {
+        if(count!=null)
+        {
+            count.text = "" + s;
+        }
+    }
+
+}
